7addbec5486665854f6dc46549bb0b988536e186
moksgo <gowthammokshagundam@gmail.com>
2020-09-28 23:25:11 -0400

    one more commit

--- main.tf
+++ main.tf
@@ -45,6 +45,20 @@ resource "aws_security_group" "default" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+    ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3001
+    to_port     = 3001
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
    egress {
     from_port   = 80
     to_port     = 80
